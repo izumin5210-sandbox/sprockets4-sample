93a11884e36e3a3b01ca735e7ae1b903b62c974a
izumin5210 <masayuki@izumin.info>
2016-09-14 22:57:31 +0900

    :wrench: Setup npm modules

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module RailsWithModernFrontendStack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.browserify_rails.commandline_options = "-t [ babelify ]"
+    config.browserify_rails.source_map_environments << "development"
   end
 end
